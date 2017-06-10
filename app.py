918227eeb44a47e76508d52fbe9da5c7f4986021
Densha1053 <a2p2i2t1@icloud.com>
2017-06-10 17:18:47 +0700

    Create app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from urllib.error import HTTPError
 import json
 import os
 
+import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.client as mqtt
+
 from flask import Flask
 from flask import request
 from flask import make_response
@@ -34,19 +37,48 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: " + str(rc))
+
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+def on_publish(mqttc, obj, mid):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
 
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    mqttc = mqtt.Client()
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+    # Uncomment to enable debug messages
+    # mqttc.on_log = on_log
+    mqttc.username_pw_set("Benz1053","benz1053")
+    mqttc.connect("km1.io", 1883, 60)
+    mqttc.subscribe("/Benz1053/room2”, 0)
+    mqttc.loop_forever()
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+    print("tuple")
+    (rc, mid) = mqttc.publish("/Benz1053/room2", yql_query, qos=2)
+    res = makeWebhookResult()
     return res
+
 
 
 def makeYqlQuery(req):
@@ -56,36 +88,11 @@ def makeYqlQuery(req):
     if city is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return city
 
 
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+def makeWebhookResult():
+    speech = "ทำให้แล้วนะ"
 
     print("Response:")
     print(speech)
