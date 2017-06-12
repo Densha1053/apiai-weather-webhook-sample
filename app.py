0ccfcbb0fc3efc93f603dea19854ecb8d9996df1
Densha1053 <a2p2i2t1@icloud.com>
2017-06-12 16:26:08 +0700

    Create app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from urllib.error import HTTPError
 
 import json
 import os
+import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.client as mqtt
 
 from flask import Flask
 from flask import request
@@ -84,6 +86,8 @@ def makeWebhookResult(data):
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    print("tuple")
+    (rc, mid) = mqttc.publish("/Benz1053/room2", speech, qos=2)
 
     print("Response:")
     print(speech)
@@ -95,7 +99,35 @@ def makeWebhookResult(data):
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: " + str(rc))
 
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+def on_publish(mqttc, obj, mid):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+mqttc.on_log = on_log
+mqttc.username_pw_set("Benz1053","benz1053")
+mqttc.connect("km1.io", 1883, 60)
+mqttc.subscribe("/Benz1053/room2”, 2)
+mqttc.loop_forever()
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
