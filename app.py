f6eb7adc4457b3d4de7865ea7975211915d12c9a
Densha1053 <a2p2i2t1@icloud.com>
2017-06-12 16:20:38 +0700

    Create app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
@@ -10,9 +8,6 @@ from urllib.error import HTTPError
 
 import json
 import os
-
-import context  # Ensures paho is in PYTHONPATH
-import paho.mqtt.client as mqtt
 
 from flask import Flask
 from flask import request
@@ -39,29 +34,56 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "MQTT":
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    payload = {
-        "city" : city
-    }
-    baseurl = "https://api.join.me/v1/meetings"
-    p = Request(baseurl)
-    p.add_header('Content-Type', 'application/json; charset=utf-8')
-    p.add_header('Authorization', 'Bearer ' + access_token) #from oauth
-    jsondata = json.dumps(payload)
-    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-    jresult = urlopen(p, jsondataasbytes).read()
-    data = json.loads(jresult)
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
     res = makeWebhookResult(data)
     return res
 
 
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
 
 def makeWebhookResult(data):
-    speech = "ok"
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
@@ -69,8 +91,8 @@ def makeWebhookResult(data):
     return {
         "speech": speech,
         "displayText": speech,
-        "data": {},
-        "contextOut": [],
+        # "data": data,
+        # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
 
