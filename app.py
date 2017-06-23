f67422aee57f5b9068528290955cc6b8ea4ef394
Densha1053 <a2p2i2t1@icloud.com>
2017-06-23 16:23:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,24 +16,7 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
-def on_connect(mqttc, obj, flags, rc):
-    print("rc: " + str(rc))
 
-
-def on_message(mqttc, obj, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-
-def on_publish(mqttc, obj, mid):
-    print("mid: " + str(mid))
-
-
-def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-
-def on_log(mqttc, obj, level, string):
-    print(string)
 # Flask app should start in global layout
 app = Flask(__name__)
 
